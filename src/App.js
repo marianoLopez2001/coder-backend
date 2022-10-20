114ab0663b6746afd224e054c67741194e4c2b65
Mariano Lopez <lopezmariano3108@gmail.com>
2022-10-20 13:50:08 -0300

    termine el desafio de clases

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  class Usuario {
+    constructor(nombre, apellido) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.libros = []
+      this.mascotas = []
+    }
+
+    getFullName() {
+      return `${this.nombre} ${this.apellido}`;
+    };
+
+    addMascota(mascota) {
+      this.mascotas.push(mascota)
+    }
+
+    countMascotas() {
+      return `Número de mascotas: ${this.mascotas.length}`
+    }
+
+    addBook(nombre, autor) {
+      this.libros.push({ nombre: nombre, autor: autor })
+    }
+
+    getBookNames() {
+      const arrayNombresLibros = []
+
+      this.libros.map(i => {
+        return (arrayNombresLibros.push(i.autor))
+      })
+      return arrayNombresLibros
+    }
+  };
+
+  const usuario1 = new Usuario("mariano", "lopez")
+
+  usuario1.addBook("La Odisea", "Homero")
+  usuario1.addBook("Hamlet", "Shakespeare")
+  usuario1.addMascota("Perrito")
+
+  console.log(usuario1)
+  console.log(usuario1.getBookNames());
+  console.log(usuario1.countMascotas());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <p>coder-backend</p>
+    </>
+  )
 }
 
 export default App;
